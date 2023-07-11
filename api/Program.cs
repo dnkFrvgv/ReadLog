70b38f408f78fa128c699b34acfffcffc0907441
Dominique <carinefrvga@gmail.com>
2023-07-11 11:58:32 -0300

    add connection string and sync migrations

--- api/Program.cs
+++ api/Program.cs
@@ -1,9 +1,18 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(opt => {
+
+	opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+});
 
 var app = builder.Build();
 
@@ -16,5 +25,19 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+try
+{
+	var context = services.GetRequiredService<DataContext>();
+	await context.Database.MigrateAsync();
+}
+catch (Exception e)
+{
+	var logger = services.GetRequiredService<ILogger<Program>>();
+	logger.LogError(e, "Error migration");
+}
 
 app.Run();
